9bb7f81ceda6ea2db58cb2fb961dd8cda5a062e7
summer-o3o <ho_ho95@naver.com>
2024-01-03 11:10:03 +0900

    add : 세션1 (모달 & 페이지 라우터)

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -1,10 +1,12 @@
 import {ReactNode} from "react";
+import styles from '@/app/(beforeLogin)/_component/main.module.css';
 
 type Props = { children: ReactNode, modal: ReactNode };
-export default function Layout({ children }: Props) {
+export default function Layout({ children, modal }: Props) {
   return (
-    <div>
+    <div className={styles.container}>
       {children}
+      {modal}
     </div>
   )
 }
